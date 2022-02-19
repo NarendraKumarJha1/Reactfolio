6e8e0125697d7a32acbe846408f5f89e414f9655
narendra kumar jha <56018997+NarendraKumarJha1@users.noreply.github.com>
2022-02-20 02:58:07 +0530

    Setup navbar and projects section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,18 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome to <br/> 
+        My Portfolio
+      </SectionTitle>
+      <SectionText>
+        The purpose of this website is to showcase my skills in brief.
+      </SectionText>
+      <Button onClick={()=>window.location='htttps://google.com'} >Learn more</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
