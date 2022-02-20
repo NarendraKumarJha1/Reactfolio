fdf6ec785c44908e510226d457bfeabf7357a016
narendra kumar jha <56018997+NarendraKumarJha1@users.noreply.github.com>
2022-02-20 22:49:40 +0530

    Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,9 +6,32 @@ import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTi
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+   <FooterWrapper>
+     <LinkList>
+     <LinkColumn>
+     <LinkTitle>Call</LinkTitle>
+     <LinkItem href='tel:111-111-111'>111-111-111</LinkItem>
+     </LinkColumn>
+     <LinkColumn>
+     <LinkTitle>Email</LinkTitle>
+     <LinkItem href='mailto:jhavicky25@gmail.com'>jhavicky25@gmail.com</LinkItem>
+     </LinkColumn>
+     </LinkList>
+     <SocialContainer>
+       <CompanyContainer>
+         <Slogan>Innovating one project at a time</Slogan>
+       </CompanyContainer>
+       <SocialIcons href='https://github.com'>
+       <AiFillGithub/>
+     </SocialIcons>
+     <SocialIcons href='https://linkedin.com'>
+       <AiFillLinkedin/>
+     </SocialIcons>
+     <SocialIcons href='https://github.com'>
+       <AiFillInstagram/>
+     </SocialIcons>
+     </SocialContainer>
+   </FooterWrapper>
   );
 };
 
